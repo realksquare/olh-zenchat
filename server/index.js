f26f24f58d45555e6889989bc18912a86f67a200
realksquare <onlinelearninghubteam@gmail.com>
2026-04-24 23:36:00 +0530

    Refactor: Secure CORS settings

--- server/index.js
+++ server/index.js
@@ -23,8 +23,8 @@ const allowedOrigins = [
 
 const io = new Server(server, {
     cors: {
-        origin: (origin, callback) => callback(null, true),
-        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        origin: allowedOrigins,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
         credentials: true
     },
 });
@@ -32,7 +32,13 @@ const io = new Server(server, {
 app.set("io", io);
 
 app.use(cors({ 
-    origin: (origin, callback) => callback(null, true),
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept"]
